7f29e3a6c4f3e310c85b904c626cb32c33278b95
whichguy <whichguy@users.noreply.github.com>
2025-08-06 12:09:30 -0700

    test: add test case file for comprehensive testing

---
+++ test-case-file.js
@@ -0,0 +1,5 @@
+// Test case file for comprehensive agentwatch testing
+function testCase() {
+  console.log('Testing agentwatch pattern persistence');
+  console.log('This file should be auto-detected in future PRs');
+}
