7f6853b92fceedc25f82f8f9d31b80363eba596c
whichguy <whichguy@users.noreply.github.com>
2025-08-06 06:46:54 -0700

    Add test file for AgentWatch demonstration
    This file contains various code patterns for testing:
    - console.log statements (lint should flag)
    - var usage (lint should suggest const/let)
    - == comparison (lint should suggest ===)
    - TODO comment (lint should detect)
    - Function definition (promptexpert can analyze)
    
    Perfect for demonstrating AgentWatch agents:
    - @agentwatch echo preview
    - @agentwatch lint
    - @agentwatch promptexpert programming

---
+++ test-file.js
@@ -0,0 +1,10 @@
+console.log('Hello AgentWatch! This is a test file.');
+
+function testFunction() {
+  var message = "Testing AgentWatch functionality";
+  console.log(message);
+  return message == "Testing AgentWatch functionality";
+}
+
+// TODO: Add more comprehensive tests
+testFunction();
